045f987d4353360091ea1c8db64f1be028aa0273
misha <mishanchik.yushkevich@gmail.com>
2025-03-28 20:55:14 +0100

    Basic algorithms and class Photo

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include "photo.hpp"
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        cerr << "Usage: program.exe <path to photo>" << endl;
+        return 1;
+    }
+
+    string path = argv[1];
+
+    Photo image(path);
+    image.show("Original image");
+    image.showHist();
+    image.showHE();
+    image.showCLAHE();
+    image.showGC();
+    
+    waitKey(0);
+    return 0;
+}
